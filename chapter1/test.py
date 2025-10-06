22b758e58c5b3c77d9ed2bd6da9de7141188a401
Shahriar Hassan <shs102030405060@gmail.com>
2025-10-06 12:09:53 +0600

    included venv activation code from powershell

--- chapter1/test.py
+++ chapter1/test.py
@@ -1,1 +1,8 @@
 a = 3
+"""
+# from your PS terminal
+cd L:\PC\lidar_odometry
+Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass   # temporary for this terminal
+.\.venv\Scripts\Activate.ps1
+"""
+print(a)
